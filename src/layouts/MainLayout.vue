ecc5f4e17c57b65781758a224a7212995cadbaa2
SuyeonSun <suninisy@gmail.com>
2023-04-21 10:55:54 +0900

    메인 레이아웃 디자인 수정

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,54 +11,35 @@ const movePage = (path) => {
 </script>
 
 <template>
-  <div class="q-pa-md">
-    <q-layout view="lHh Lpr lff" container style="height: 100vh" class="shadow-2 rounded-borders">
-      <q-header elevated class="bg-cyan-8">
+    <q-layout view="hHh Lpr lff" class="shadow-2 rounded-borders">
+      <q-header class="bg-white" bordered>
         <q-toolbar>
-          <q-toolbar-title>Header</q-toolbar-title>
-          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-btn flat @click="drawer = !drawer" round dense icon="home" color="black" />
         </q-toolbar>
       </q-header>
 
       <q-drawer
         v-model="drawer"
         show-if-above
-        :width="200"
-        :breakpoint="400"
+        :width="55"
+        :breakpoint="500"
+        bordered
       >
-        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
-          <q-list padding>
+        <q-scroll-area class="fit">
+          <q-list padding class="menu-list">
             <q-item clickable v-ripple @click="movePage('/')">
               <q-item-section avatar>
-                <q-icon name="star" />
-              </q-item-section>
-
-              <q-item-section>
-                Main
+                <q-icon name="menu" />
               </q-item-section>
             </q-item>
 
             <q-item clickable v-ripple @click="movePage('/new')">
               <q-item-section avatar>
-                <q-icon name="inbox" />
-              </q-item-section>
-
-              <q-item-section>
-                Write
+                <q-icon name="send" />
               </q-item-section>
             </q-item>
           </q-list>
         </q-scroll-area>
-
-        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            <div class="text-weight-bold">Razvan Stoenescu</div>
-            <div>@rstoenescu</div>
-          </div>
-        </q-img>
       </q-drawer>
 
       <q-page-container>
@@ -67,5 +48,9 @@ const movePage = (path) => {
         </q-page>
       </q-page-container>
     </q-layout>
-  </div>
 </template>
+
+<style lang="sass" scoped>
+.menu-list .q-item
+  border-radius: 0 32px 32px 0
+</style>
